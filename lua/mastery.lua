2227c51330b0650ee4918c2f19c7c4c105f04376
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-05-15 09:17:39 -0500

    start and stop times

--- lua/mastery.lua
+++ lua/mastery.lua
@@ -1,12 +1,26 @@
 local M = {}
 
 M.setup = function()
-	local now = os.time()
-	local fp = vim.fn.stdpath("data") .. "/mastery.log"
-	local log = io.open(fp, "a")
-	if log then
-		log:write(now, "\n")
-	end
+	vim.api.nvim_create_autocmd("VimEnter", {
+		callback = function()
+			local now = os.time()
+			local fp = vim.fn.stdpath("data") .. "/mastery.log"
+			local log = io.open(fp, "a")
+			if log then
+				log:write(now, ",")
+			end
+		end,
+	})
+	vim.api.nvim_create_autocmd("VimLeave", {
+		callback = function()
+			local now = os.time()
+			local fp = vim.fn.stdpath("data") .. "/mastery.log"
+			local log = io.open(fp, "a")
+			if log then
+				log:write(now, "\n")
+			end
+		end,
+	})
 end
 
 return M
