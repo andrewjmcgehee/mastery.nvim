f4ae6f51ca1cc79ea63cb70c631880d764c2c383
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-05-15 09:27:01 -0500

    add state

--- lua/mastery.lua
+++ lua/mastery.lua
@@ -3,6 +3,18 @@ local M = {}
 M.setup = function()
 	vim.api.nvim_create_autocmd("VimEnter", {
 		callback = function()
+			-- read or create the state file
+			local file = io.open(vim.fn.stdpath("data") .. "/mastery.state", "r")
+			local offset = 0
+			local elapsed = 0
+			if not file then
+				file = io.open(vim.fn.stdpath("data") .. "/mastery.state", "w")
+			else
+				offset = tonumber(file:read("*1"), 10)
+				elapsed = tonumber(file:read("*1"), 10)
+			end
+			vim.notify(offset .. " " .. elapsed)
+			-- log the time
 			local now = os.time()
 			local fp = vim.fn.stdpath("data") .. "/mastery.log"
 			local log = io.open(fp, "a")
