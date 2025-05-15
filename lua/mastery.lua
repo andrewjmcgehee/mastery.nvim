4ba44dcc2854c41724bdee3846eddca901097c84
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-05-14 23:40:37 -0500

    formatting

--- lua/mastery.lua
+++ lua/mastery.lua
@@ -2,10 +2,10 @@ local M = {}
 
 M.setup = function()
 	local now = os.time()
-	local fp = vim.fn.stdpath("data") .. "/mastery"
+	local fp = vim.fn.stdpath("data") .. "/mastery.log"
 	local log = io.open(fp, "a")
 	if log then
-		log:write(now)
+		log:write(now, "\n")
 	end
 end
 
