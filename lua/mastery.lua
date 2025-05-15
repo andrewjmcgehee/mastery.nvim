9419054b714549be968d1e475c6e2a6ae6fcecee
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-05-15 10:23:47 -0500

    refactor to be module functions; remove offset

--- lua/mastery.lua
+++ lua/mastery.lua
@@ -1,68 +1,89 @@
 local M = {}
 
-local state = {
+M.session_state = {
 	enter_ts = -1,
 	leave_ts = -1,
 }
 
-M.state = state
+M.log_enter_ts = function()
+	-- log and set the enter time
+	local now = os.time()
+	M.state.enter_ts = now
+	local log_fp = vim.fn.stdpath("data") .. "/mastery.log"
+	local log_file = io.open(log_fp, "a")
+	if log_file then
+		log_file:write(now, ",")
+		log_file:close()
+	end
+	return now
+end
+
+M.log_exit_ts = function()
+	local now = os.time()
+	local log_fp = vim.fn.stdpath("data") .. "/mastery.log"
+	local log_file = io.open(log_fp, "a")
+	if log_file then
+		log_file:write(now, "\n")
+		log_file:close()
+	end
+	return now
+end
+
+M.create_or_read_global_state_file = function()
+	-- read or create the state file
+	local elapsed = 0
+	local state_fp = vim.fn.stdpath("data") .. "/mastery.state"
+	local state_file = io.open(state_fp, "r")
+	if not state_file then
+		state_file = io.open(state_fp, "w")
+		if not state_file then
+			vim.notify("failed to create mastery.state file", vim.log.levels.ERROR, { title = "Mastery" })
+			return
+		end
+		state_file:write(elapsed .. "\n")
+	else
+		elapsed = state_file:read("*n")
+	end
+	state_file:close()
+	return {
+		elapsed = elapsed,
+	}
+end
+
+M.update_global_state_file = function(elapsed)
+	local state_fp = vim.fn.stdpath("data") .. "/mastery.state.new"
+	local state_file = io.open(state_fp, "w")
+	if not state_file then
+		vim.notify("failed to create mastery.state file", vim.log.levels.ERROR, { title = "Mastery" })
+		return
+	end
+	state_file:write(elapsed .. "\n")
+end
+
+M.on_enter = function()
+	M.log_enter_ts()
+end
+
+M.on_leave = function()
+	local now = M.log_exit_ts()
+	local diff = now - M.session_state.enter_ts
+	local state = M.create_or_read_global_state_file()
+	if not state then
+		return
+	end
+	M.update_global_state_file(state.elapsed + diff)
+end
 
 M.setup = function()
-	vim.api.nvim_create_autocmd("VimEnter", {
-		callback = function()
-			-- log and set the enter time
-			local now = os.time()
-			M.state.enter_ts = now
-			local log_fp = vim.fn.stdpath("data") .. "/mastery.log"
-			local log_file = io.open(log_fp, "a")
-			if log_file then
-				log_file:write(now, ",")
-				log_file:close()
-			end
-		end,
-	})
-	vim.api.nvim_create_autocmd("VimLeavePre", {
-		callback = function()
-			-- write time to log
-			local now = os.time()
-			local log_fp = vim.fn.stdpath("data") .. "/mastery.log"
-			local log_file = io.open(log_fp, "a")
-			if log_file then
-				log_file:write(now, "\n")
-				log_file:close()
-			end
-			-- read or create the state file
-			local offset = 0
-			local elapsed = 0
-			local state_fp = vim.fn.stdpath("data") .. "/mastery.state"
-			local state_file = io.open(state_fp, "r")
-			if not state_file then
-				state_file = io.open(state_fp, "w")
-				if not state_file then
-					vim.notify("failed to create mastery.state file", vim.log.levels.ERROR, { title = "Mastery" })
-					return
-				end
-				state_file:write(offset .. "\n")
-				state_file:write(elapsed .. "\n")
-			else
-				offset = state_file:read("*n")
-				elapsed = state_file:read("*n")
-			end
-			state_file:close()
-			-- calculate diff and update global state file
-			local diff = now - M.state.enter_ts
-			local total_elapsed = offset + elapsed + diff
-			vim.notify(total_elapsed .. "s", vim.log.levels.INFO, { title = "Mastery" })
-			-- write new state
-			state_file = io.open(state_fp, "w")
-			if state_file then
-				state_file:write(offset .. "\n")
-				state_file:write((elapsed + diff) .. "\n")
-			else
-				vim.notify("failed to write mastery.state file", vim.log.levels.ERROR, { title = "Mastery" })
-			end
-		end,
-	})
+	vim.api.nvim_create_autocmd("VimEnter", { callback = M.on_enter })
+	vim.api.nvim_create_autocmd("VimLeave", { callback = M.on_leave })
+	vim.api.nvim_create_user_command("Mastery", function()
+		local state = M.create_or_read_global_state_file()
+		if not state then
+			return
+		end
+		vim.notify("you've achieved " .. state.elapsed .. "s of mastery", vim.log.levels.INFO, { title = "Mastery" })
+	end, {})
 end
 
 return M
